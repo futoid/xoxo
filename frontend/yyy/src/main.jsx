d57056342d02b610cc64360ab289fd9a40f48c6f
Aditya <kumar.aditya3340@gmail.com>
2025-05-18 01:16:34 +0530

    modified: removed unnecessary files and added required dependencies

--- frontend/yyy/src/main.jsx
+++ frontend/yyy/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Home from './pages/Home'
+import About from './pages/About'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ frontend/yyy/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Learn more about our application.</p>
+    </div>
+  )
+}
+
+export default About 
